b3e34b5725e52f8e765a38bf1966f94a381708a0
Kevin Santiago Rey <ksreyr@gmail.com>
2024-01-26 09:57:10 +0100

    feat(ms): improve interface return types

--- books/src/main/java/com/unir/webdev/books/application/ChangeAvailabilityUseCase.java
+++ books/src/main/java/com/unir/webdev/books/application/ChangeAvailabilityUseCase.java
@@ -1,15 +1,17 @@
 package com.unir.webdev.books.application;
 
 import com.unir.webdev.books.domain.repository.BookRepository;
-import com.unir.webdev.books.domain.response.Result;
+import io.vavr.collection.List;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
 
 @Service
 @RequiredArgsConstructor
@@ -17,15 +19,22 @@ import java.util.UUID;
 public class ChangeAvailabilityUseCase {
     BookRepository bookRepository;
 
-    public Result<String, Object> changeAvailability(List<UUID> booksID) {
-        return booksID.stream()
-                      .filter(bookRepository :: isValidBook)
-                      .map(this :: changeAvailabilityOf)
-                      .allMatch(Result :: isSuccess) ? Result.success("all changed") :
-               Result.error("Error change");
+    public Either<String, Boolean> changeAvailability(List<UUID> booksID) {
+        return List.ofAll(booksID)
+                   .filter(bookRepository :: isValidBook)
+                   .map(this :: changeAvailabilityOf)
+                   .foldLeft(Either.right(true), validateNotErrorAtProcess());
+
+
     }
 
-    private @NotNull Result<String, Object> changeAvailabilityOf(UUID uuids) {
-        bookRepository.changeAvailabilityOf(uuids); return Result.success("change");
+    @NotNull
+    private static BiFunction<Either<String, Boolean>, Either<String, Void>, Either<String, Boolean>> validateNotErrorAtProcess() {
+        return (acc, either) -> acc.isLeft() ? acc : either.isLeft() ? Either.left(either.getLeft()) : acc;
+    }
+
+    private @NotNull Either<String, Void> changeAvailabilityOf(UUID uuid) {
+        return Try.run(() -> bookRepository.changeAvailabilityOf(uuid))
+                  .toEither("Availability do not changed");
     }
 }
--- books/src/main/java/com/unir/webdev/books/infrastructure/controllers/ChangeAvailabilityBookController.java
+++ books/src/main/java/com/unir/webdev/books/infrastructure/controllers/ChangeAvailabilityBookController.java
@@ -1,19 +1,17 @@
 package com.unir.webdev.books.infrastructure.controllers;
 
 import com.unir.webdev.books.application.ChangeAvailabilityUseCase;
-import com.unir.webdev.books.domain.response.Result;
 import com.unir.webdev.books.infrastructure.controllers.DTO.request.ChangeAvailabilityRequest;
+import io.vavr.collection.List;
+import io.vavr.control.Option;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,20 +22,14 @@ public class ChangeAvailabilityBookController {
 
     @PostMapping ("/changeAvailability")
     public ResponseEntity<?> handle(@RequestBody ChangeAvailabilityRequest changeAvailabilityRequest) {
-        return Optional.ofNullable(changeAvailabilityRequest)
-                       .filter(ChangeAvailabilityRequest :: existBooks)
-                       .map(ChangeAvailabilityRequest :: booksID)
-                       .map(changeAvailabilityUseCase :: changeAvailability)
-                       .map(ChangeAvailabilityBookController :: buildResponse)
-                       .orElse(ResponseEntity.badRequest()
-                                                      .body("Bad Request given "));
-    }
+        return Option.of(changeAvailabilityRequest)
+                     .filter(ChangeAvailabilityRequest :: existBooks)
+                     .map(ChangeAvailabilityRequest :: booksID)
+                     .map(uuids -> List.ofAll(uuids))
+                     .map(changeAvailabilityUseCase :: changeAvailability)
+                     .map(booleans -> booleans.isLeft() ? ResponseEntity.badRequest()
+                                                                        .body(booleans.getLeft()) : ResponseEntity.ok("availability changed"))
+                     .get();
 
-    @NotNull
-    private static ResponseEntity<Object> buildResponse(Result<String, Object> stringObjectsResult) {
-        return stringObjectsResult.isSuccess() ?
-               ResponseEntity.ok(stringObjectsResult.getSuccess()) :
-               ResponseEntity.badRequest()
-                             .body(stringObjectsResult.getError());
     }
 }
